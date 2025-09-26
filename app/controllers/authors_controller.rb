aca987b534e296ecfa7d3f3cab99fbffda09da2a
John Trocchi <johntrocchi@macbookpro.mynetworksettings.com>
2025-09-25 21:28:57 -0400

    add forms to authors new page

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,29 @@
+class AuthorsController < ApplicationController
+    def index
+        @authors = Author.all
+    end
+
+    def show
+        @author = Author.find(params[:id])
+    end
+
+    def new
+        @author = Author.new
+    end
+
+    def create
+        validate_points
+        @author = Author.new(author_params)
+      if @author.save
+          redirect_to @author, notice: "Author was successfully created."
+      else
+          render :new
+      end
+    end
+
+    private
+
+    def author_params
+        params.require(:author).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "books" => "books#index", as: :books
+
+  resources :authors, only: [:index, :show, :new, :create]
 end
